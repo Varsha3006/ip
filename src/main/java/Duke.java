c8b1b82af66ea4d0c240787bcbcad0407d9c58a1
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-08-21 18:29:43 +0800

    Week 2 ip tasks

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,4 @@
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +7,9 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
