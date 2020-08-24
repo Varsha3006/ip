528d3fff299a3c53d4c99f401d679dd1b2ab0542
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-08-24 18:16:32 +0800

    Week 3, level 1 complete

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,15 +1,17 @@
+import java.util.Scanner;
 
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("Hello! I'm Duke");
-        System.out.println("What can I do for you?");
-        System.out.println("Bye. Hope to see you again soon!");
+public class Main {
 
+    public static void start() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void exit() {
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
     }
 }
