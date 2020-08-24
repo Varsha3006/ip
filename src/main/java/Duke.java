52f06159a6fe278864e65a150bfe7b44bca5b201
LAPTOP-LK4JO5OO\65837 <varsha.ms30@gmail.com>
2020-08-24 23:38:46 +0800

    Started on Level 2: Add

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,23 @@
+
 import java.util.Scanner;
 
 
 public class Duke {
+
+
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+
 
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
         String command = in.nextLine();
         while (command.equals("bye") == false) {
-            System.out.println(command);
+            System.out.println("added: " + command);
             command = in.nextLine();
         }
-            System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
 
     }
 }
-
-
